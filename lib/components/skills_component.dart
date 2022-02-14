68e788513195183ce2f9080fb335bf33876ccc26
sk25469 <sk25469@gmail.com>
2022-02-14 09:53:30 +0530

    animation working

--- lib/components/skills_component.dart
+++ lib/components/skills_component.dart
@@ -2,8 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/constants/custom_theme.dart';
 import 'package:rive/rive.dart';
 
-class SkillComponent extends StatelessWidget {
+class SkillComponent extends StatefulWidget {
   const SkillComponent({Key? key}) : super(key: key);
+
+  @override
+  State<SkillComponent> createState() => _SkillComponentState();
+}
+
+class _SkillComponentState extends State<SkillComponent> {
+  late RiveAnimationController _controller;
+
+  /// Is the animation currently playing?
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = OneShotAnimation(
+      'Appearing',
+      autoplay: false,
+      onStop: () => setState(() => _isPlaying = false),
+      onStart: () => setState(() => _isPlaying = true),
+    );
+    // _controller.isActive = true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +38,18 @@ class SkillComponent extends StatelessWidget {
         theme,
       ),
       child: Stack(
-        children: const [
+        children: [
           Positioned(
-            child: RiveAnimation.asset('assets/animation/upload-animation.riv'),
-            bottom: 0,
-            left: 10,
-          )
+            child: RiveAnimation.asset(
+              'assets/animation/upload-animation.riv',
+              animations: const ['Appearing'],
+              controllers: [_controller],
+            ),
+            top: 40,
+            left: 40,
+            width: 500,
+            height: 500,
+          ),
         ],
       ),
     );
